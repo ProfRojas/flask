ae99bd9db5001863331581297ce283a19b11aa07
Carlos Rojas <carlos.rojas@sjsu.edu>
2023-03-10 11:54:44 -0800

    Add variables to templates

---
+++ 04_templates_with_variables/app/routes.py
@@ -0,0 +1,26 @@
+from app import myapp_obj
+from flask import escape
+from flask import render_template
+
+@myapp_obj.route("/")
+@myapp_obj.route("/index.html")
+def index():
+    name = 'Carlos'
+    books = [ {'author': 'authorname1',
+                'book':'bookname1'},
+             {'author': 'authorname2',
+              'book': 'bookname2'}]
+    return render_template('hello.html',name=name, books=book)
+
+@myapp_obj.route("/hello")
+def hello():
+    return "Hello World!"
+
+@myapp_obj.route("/login")
+def login():
+    return "Login Page!"
+
+@myapp_obj.route("/members/<string:name>/")
+def getMember(name):
+    return escape(name)
+
